5c3c88e266e1512b1962bc32daa2eb7c5e1f4bc3
Tom Lloyd <c00231308@itcarlow.ie>
2020-10-05 15:33:39 +0100

    XOR21 Linked

---
+++ RWM-P0-c00231308-Combined/Assets/Plugins/XOR21/XOR21.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XOR21
+{
+  public static int XOR(int x)
+  {
+        if(x > int.MaxValue)
+        {
+            return x;
+        }
+
+        return x ^ 21;
+  }
+
+  public static int[] XORcalculation(int[] xs)
+  {
+    int[] result = new int[xs.Length];
+    for (int i = 0; i < xs.Length; i++)
+    {
+      result[i] = XOR(xs[i]);
+    }
+    return result;
+  }
+    public static long XORLong(long x)
+    {
+        if (x > int.MaxValue)
+        {
+            return x;
+        }
+
+        return x ^ 21;
+    }
+    public static long[] XORcalculationLong(long[] xs)
+    {
+        long[] result = new long[xs.Length];
+        for (long i = 0; i < xs.Length; i++)
+        {
+            result[i] = XORLong(xs[i]);
+        }
+        return result;
+    }
+}
+
