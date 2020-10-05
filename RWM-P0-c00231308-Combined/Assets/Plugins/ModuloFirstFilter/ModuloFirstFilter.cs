acedff2a8435da0c63ce20d5134adf308cee1d84
Tom Lloyd <c00231308@itcarlow.ie>
2020-10-05 15:43:31 +0100

    ModuloFirstFilter added

---
+++ RWM-P0-c00231308-Combined/Assets/Plugins/ModuloFirstFilter/ModuloFirstFilter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuloFirstFilter : MonoBehaviour
+{
+    public static int moduloFirst(int x, int first)
+    {
+        return x % first;
+    }
+
+    public static int[] results(int[] xs)
+    {
+        int[] result = new int[xs.Length];
+        for (int i = 0; i < xs.Length; i++)
+        {
+            result[i] = moduloFirst(xs[i], xs[0]);
+        }
+        return result;
+    }
+}
