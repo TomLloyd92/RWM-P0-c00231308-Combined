763e980c6fec9ae874abb3cc0284333c2757393a
Tom Lloyd <c00231308@itcarlow.ie>
2020-10-05 15:53:13 +0100

    Started Combined Filter

---
+++ RWM-P0-c00231308-Combined/Assets/Scipts/CombinedFilter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CombinedFilter
+{
+    public static int[] combinedFilter(int[] xs)
+    {
+        return XOR21.XORcalculation(results.moduloFirst(xs));
+    }
+}
